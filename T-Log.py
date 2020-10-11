d3b828c6642bf1c88ae36004d01b9ad4b7ad4b68
Shoaib <72677434+Mux-Hackers@users.noreply.github.com>
2020-10-11 07:05:25 +0500

    Create T-Log.py

---
+++ T-Log.py
@@ -0,0 +1,13 @@
+# T-Log
+INSTALLATION and use
+
+1. `git clone https://github.com/Mux-Hackers/T-Log.git`
+
+2. `cd Login`
+
+3. `python login.py`
+
+`Now you want to set your username and password`
+
+
+for more information visit to my Youtube Channel Mux Hackers. 
